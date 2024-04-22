ee9f963beaaabea9a0186d6060170d65633a8452
Iryna2110 <151244931+Iryna2110@users.noreply.github.com>
2024-04-22 16:43:15 +0300

    update homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,25 @@
-function filterArray(numbers, value) {
-    let newArray = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-    for (const number of numbers) {
-        if (number > value) {
-            newArray.push(number);
-        }
-    }
-    return newArray
-    }
-    
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+
+  changeUserName(newUsername) {
+    profile.username = newUsername;
+  },
+
+updatePlayTime(newPlayTime) {
+    profile.playTime = profile.playTime + newPlayTime;
+  },
+}
 
 
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUserName("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
